d7db8455466ed54c1f1ddbb9da454d7dd476cff3
Eduardo Portet <eduardoportet@gmail.com>
2018-09-20 02:54:38 -0700

    finally render components iteratively

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import {Container, Input, Button, InputGroup, InputGroupAddon, FormGroup, Label,
 import './App.css';
 
 class ExplorerComponent extends Component {
-	constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
+	handleSubmit(event) {
+		event.preventDefault();
+		const data = new FormData(event.target);
 
-    fetch('/api/form-submit-url', {
-      method: 'POST',
-      body: data,
-    });
-  }
+		fetch('/api/form-submit-url', {
+			method: 'POST',
+			body: data,
+		});
+	}
 
 	render() {
 		return (
@@ -24,21 +24,21 @@ class ExplorerComponent extends Component {
 			<Card>
 				<CardHeader>{this.props.title}</CardHeader>
 				<CardBody>
-		  		<CardSubtitle>{this.props.method}</CardSubtitle>
-		  		<Label>URL: <span>{this.props.url}</span></Label>
-		  		<FormGroup onSubmit={this.handleSubmit}>
-		  			<Label htmlFor="email">Email</Label>
-		  			<Input name="email" type="email" />
-		  			<Label >Full Name</Label>
-		  			<Input htmlFor="email"/>
-		  			<Label>Phone</Label>
-		  			<Input />
-		  		</FormGroup>
-		  		<Button color="success">Send Request</Button>
-		  		<CardSubtitle>Response</CardSubtitle>
-		  		<FormGroup>
-		  			<Input type="textarea" readOnly />
-		  		</FormGroup>
+					<CardSubtitle>{this.props.method}</CardSubtitle>
+					<Label>URL: <span>{this.props.url}</span></Label>
+					<FormGroup onSubmit={this.handleSubmit}>
+						<Label htmlFor="email">Email</Label>
+						<Input name="email" type="email" />
+						<Label >Full Name</Label>
+						<Input htmlFor="email"/>
+						<Label>Phone</Label>
+						<Input />
+					</FormGroup>
+					<Button color="success">Send Request</Button>
+					<CardSubtitle>Response</CardSubtitle>
+					<FormGroup>
+						<Input type="textarea" readOnly />
+					</FormGroup>
 				</CardBody>
 			</Card>
 		</div>
@@ -46,33 +46,61 @@ class ExplorerComponent extends Component {
 	}
 }
 
-class App extends Component {
+class APIExplorer extends Component {
 	constructor() {
 		super();
 		this.valueJSON = 'https://raw.githubusercontent.com/eportet/api-explorer/master/public/endpoints.json';
 		this.state = {}
 	}
 
-	componentDidMount() {
+	componentWillMount() {
 		fetch(this.valueJSON)
-		.then(response => response.json())
-		.then(json => {
-			this.setState(json);
-			console.log(this.state);
-		})
+		.then(res => res.json())
+		.then(endpoints => {
+			this.setState({endpoints});
+		});
 	}
+
+	renderEndpoints() {
+		if (this.state.endpoints === undefined) {
+			return null;
+		}
+
+		function createEndpoints(endpoints) {
+			const ENDPOINTS = endpoints.map((endpoint) =>
+				<ExplorerComponent key={endpoint.title} title={endpoint.title} method={endpoint.method} url={endpoint.url} />
+			);
+
+			return (ENDPOINTS);
+		}
+
+		return (
+			<div>
+				{createEndpoints(this.state.endpoints)}
+			</div>
+		);
+	}
+
 
 	render() {
 		return (
+			<div>
+				<InputGroup className="download-url-wrapper">
+					<InputGroupAddon addonType="prepend"><Button color="primary">Smartcar API Explorer:</Button></InputGroupAddon>
+					<Input defaultValue={this.valueJSON} />
+					<InputGroupAddon addonType="append"><Button color="secondary">Explore</Button></InputGroupAddon>
+				</InputGroup>
+				{this.renderEndpoints()}
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
 			<Container>
-				<div className="download-url-wrapper">
-					<InputGroup color="secondary">
-	          <InputGroupAddon addonType="prepend"><Button color="primary">Smartcar API Explorer:</Button></InputGroupAddon>
-	          <Input defaultValue={this.valueJSON} />
-	          <InputGroupAddon addonType="append"><Button color="secondary">Explore</Button></InputGroupAddon>
-	        </InputGroup>
-        </div>
-        <ExplorerComponent title={this.state.title} method={this.state.method} url={this.state.url} />
+				<APIExplorer />
 			</Container>
 		);
 	}
